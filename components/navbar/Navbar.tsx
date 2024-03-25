71a115ada959acc3f039ab26f79523545bf6af93
David Rihacek <davidrihacekk@gmail.com>
2024-03-25 20:09:00 +0100

    new structure for NextJS

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Links from "./links/Links";
+
+function Navbar() {
+  return (
+    <div>
+      <div>Logo</div>
+      <Links />
+    </div>
+  );
+}
+
+export default Navbar;
